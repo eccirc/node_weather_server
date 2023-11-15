b79991b45e20982b1a2a140ce7c4c9cd667440a2
eccirc <davidgw@proton.me>
2023-11-15 15:27:48 +0000

    remove static port

--- public/js/app.js
+++ public/js/app.js
@@ -2,17 +2,10 @@ console.log("client side!");
 
 const title = document.getElementById("main_title")
 
-// let i = 0;
-// let sinCount = 0;
-// setInterval(() => {
-//     i += 0.01;
-//     sinCount = Math.ceil((Math.sin(i) + 1) * 100);
-//     title.style.color = `rgb(${255 - sinCount} , ${Math.ceil(i % 255)} , ${255 - sinCount})`
-// }, 10);
 
 
 const getWeatherOnClick = (address, callback) => {
-    fetch(`http://localhost:3000/weather?address=${address}`, {
+    fetch(`/weather?address=${address}`, {
     "Access-Control-Allow-Origin": true
     }).then(res => {
     res.json().then(data => {
